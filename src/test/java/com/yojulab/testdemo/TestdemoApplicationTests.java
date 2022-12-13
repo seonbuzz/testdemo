01eddb07408dad0cdf880821f60438fb2bbc7b0d
seonbuzz <ddonsdevelop@gmail.com>
2022-12-13 14:59:56 +0900

    first commit

---
+++ src/test/java/com/yojulab/testdemo/TestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yojulab.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
